b80f5eb59a6575c6ec40d68e04c0836bce841d55
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-03-24 16:28:14 -0300

    [feature] mongo atlas

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://magustincarbajal97:NXwUMBbedSFQhGID@cluster0.xkitr.mongodb.net/usersCoder?retryWrites=true&w=majority"
-  )
+  .connect("")
   .then(() => console.log("Successfull connection to database"))
   .catch((error) => console.log(`Something gone wrong. ${error}`));
